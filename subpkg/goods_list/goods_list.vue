249bb0307679f80ff002f4ebd238cb52f00e703a
林施巧 <linshiqiaode@163.com>
2023-03-12 14:04:38 +0800

    完成了商品列表页面的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
   <view>
-    goods_list
+    <view class="good-list">
+      <view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
+        <my-goods :goods=goods></my-goods>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,8 +12,63 @@
   export default {
     data() {
       return {
-        
+        queryObj: {
+          query: '',
+          cid: '',
+          pagenum: 1,
+          pagesize: 10
+        },
+        goodsList: [],
+        total: 0,
+        isLoading: false
+
       };
+    },
+    onLoad(options) {
+      this.queryObj.cid = options.cid || ''
+      this.queryObj.query = options.query || ''
+      this.getGoodsList()
+    },
+    methods: {
+      // 获取商品列表
+      async getGoodsList(cb) {
+        //打开节流阀
+        this.isLoading = true
+        const {
+          data: res
+        } = await uni.$http.get('https://api-hmugo-web.itheima.net/api/public/v1/goods/search', this
+          .queryObj)
+          cb && cb()
+          this.isLoading = false
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.goodsList = [...this.goodsList, ...res.message.goods]
+        this.total = res.message.total
+
+      },
+      onReachBottom(){
+        if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕!')
+        if(this.isLoading) return
+         // 让页码自增+1
+        this.queryObj.pagenum += 1
+        this.getGoodsList()
+      },
+      // 下拉刷新的事件
+      onPullDownRefresh() {
+        // 1. 重置关键数据
+        this.queryObj.pagenum = 1
+        this.total = 0
+        this.isloading = false
+        this.goodsList = []
+      
+        // 2. 重新发起请求
+        this.getGoodsList(() => uni.stopPullDownRefresh())
+      },
+      gotoDetail(goods){
+        //跳转到分包商品详情
+        uni.navigateTo({
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods.goods_id
+        })
+      }
     }
   }
 </script>
