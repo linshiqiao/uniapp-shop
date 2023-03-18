50791107fbf4c3c80ac44a821f8d6767e2c83ea3
林施巧 <linshiqiaode@163.com>
2023-03-18 17:22:38 +0800

    完成了登录和支付功能的开发

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
@@ -6,11 +5,14 @@ import App from './App'
 import store from './store/store.js'
 
 //导入网络请求的包
-import { $http } from '@escook/request-miniprogram'
+import {
+  $http
+} from '@escook/request-miniprogram'
+// $http.baseUrl ="https://www.esinsis.tech"
 // 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
 uni.$http = $http
 // 封装的展示消息提示的方法
-uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
   uni.showToast({
     title,
     duration,
@@ -18,13 +20,21 @@ uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
   })
 }
 // 请求开始之前做一些事情 请求拦截器
-$http.beforeRequest = function (options) {
+$http.beforeRequest = function(options) {
   uni.showLoading({
-    title:'数据加载中...'
+    title: '数据加载中...'
   })
+  // 判断请求的是否为有权限的 API 接口
+  if (options.url.indexOf('/my/') !== -1) {
+    // 为请求头添加身份认证字段
+    options.header = {
+      // 字段的值可以直接从 vuex 中进行获取
+      Authorization: store.state.m_user.token,
+    }
+  }
 }
 // 请求完成之后做一些事情 响应拦截器
-$http.afterRequest = function () {
+$http.afterRequest = function() {
   uni.hideLoading()
 }
 Vue.config.productionTip = false
@@ -32,15 +42,17 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App,
-      // 2. 将 store 挂载到 Vue 实例上
-      store,
+  ...App,
+  // 2. 将 store 挂载到 Vue 实例上
+  store,
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
--- uni_modules/my-search/components/my-settle/my-settle.vue
+++ uni_modules/my-search/components/my-settle/my-settle.vue
@@ -8,7 +8,7 @@
     <view class="amount-box">
       合计:<text class="amount">￥{{checkedGoodsAmount}}</text>
     </view>
-    <view class="btn-settle">
+    <view class="btn-settle" @click="settlement">
       结算({{checkedCount}})
     </view>
   </view>
@@ -17,16 +17,24 @@
 <script>
   import {
     mapGetters,
-    mapMutations
+    mapMutations,
+    mapState
   } from 'vuex'
   export default {
     data() {
       return {
-
+        // 倒计时的秒数
+        seconds: 3,
+        // 定时器的 Id
+        timer: null
       };
     },
     computed: {
-      ...mapGetters('m_cart', ['checkedCount', 'total','checkedGoodsAmount']),
+      ...mapGetters('m_cart', ['checkedCount', 'total', 'checkedGoodsAmount']),
+      // addstr 是详细的收货地址
+      ...mapGetters('m_user', ['addstr']),
+      // token 是用户登录成功之后的 token 字符串
+      ...mapState('m_user', ['token']),
       // 2. 是否全选
       isFullCheck() {
         return this.total === this.checkedCount
@@ -34,9 +42,75 @@
     },
     methods: {
       ...mapMutations('m_cart', ['updateAllGoodsState']),
+      // 把 m_user 模块中的 updateRedirectInfo 方法映射到当前页面中使用
+      ...mapMutations('m_user', ['updateRedirectInfo']),
       changeAllState() {
         this.updateAllGoodsState(!this.isFullCheck)
-      }
+      },
+      // 点击了结算按钮
+      settlement() {
+        // 1. 先判断是否勾选了要结算的商品
+        if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+        // 2. 再判断用户是否选择了收货地址
+        if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+        // 3. 最后判断用户是否登录了
+        // if (!this.token) return uni.$showMsg('请先登录！')
+        // 3. 最后判断用户是否登录了，如果没有登录，则调用 delayNavigate() 进行倒计时的导航跳转
+        // if (!this.token) return uni.$showMsg('请先登录！')
+        if (!this.token) return this.delayNavigate()
+      },
+      // 展示倒计时的提示消息
+      showTips(n) {
+        // 调用 uni.showToast() 方法，展示提示消息
+        uni.showToast({
+          // 不展示任何图标
+          icon: 'none',
+          // 提示的消息
+          title: '请登录后再结算！' + n + ' 秒后自动跳转到登录页',
+          // 为页面添加透明遮罩，防止点击穿透
+          mask: true,
+          // 1.5 秒后自动消失
+          duration: 1500
+        })
+      },
+      // 延迟导航到 my 页面
+      delayNavigate() {
+        // 把 data 中的秒数重置成 3 秒
+        this.seconds = 3
+        // 1. 展示提示消息，此时 seconds 的值等于 3
+        this.showTips(this.seconds)
+
+        // 1. 将定时器的 Id 存储到 timer 中
+        this.timer = setInterval(() => {
+          this.seconds--
+
+          // 2. 判断秒数是否 <= 0
+          if (this.seconds <= 0) {
+            // 2.1 清除定时器
+            clearInterval(this.timer)
+
+            // 2.2 跳转到 my 页面
+            uni.switchTab({
+              url: '/pages/my/my',
+              // 页面跳转成功之后的回调函数
+              success: () => {
+                // 调用 vuex 的 updateRedirectInfo 方法，把跳转信息存储到 Store 中
+                this.updateRedirectInfo({
+                  // 跳转的方式
+                  openType: 'switchTab',
+                  // 从哪个页面跳转过去的
+                  from: '/pages/cart/cart'
+                })
+              }
+            })
+
+            // 2.3 终止后续代码的运行（当秒数为 0 时，不再展示 toast 提示消息）
+            return
+          }
+
+          this.showTips(this.seconds)
+        }, 1000)
+      },
     }
   }
 </script>
